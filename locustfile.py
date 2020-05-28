4a8459ae1b7ebb19673b1a34dea7f95f383e5a05
Guox <33855684+dazuimao1990@users.noreply.github.com>
2020-05-28 12:19:53 +0800

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -5,10 +5,14 @@ class MyTaskSet(TaskSet):
 
     @task
     def baidu_index(self):
-        self.client.get("/")
+        self.client.get("/api/todos")
+    
+    @task
+    def baidu_index(self):
+        self.client.get("/api/req")
 
 
 class WebsiteUser(HttpLocust):
     task_set = MyTaskSet
-    min_wait = 3000     # 单位为毫秒
-    max_wait = 6000
+    min_wait = 1000     # 单位为毫秒
+    max_wait = 3000
